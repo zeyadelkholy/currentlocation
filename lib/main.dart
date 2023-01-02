e5eea62ecc25430e44f894c04043b525da9c7b42
Zeyad <zeyadelkholy>
2023-01-02 16:14:22 +0200

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'hddgff.dart';
-import 'sss.dart';
 
 
 void main() {
@@ -12,7 +10,6 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //theme: ThemeData(colorScheme: ColorScheme.light(primary: Color(0xffD2D0D5))),
       debugShowCheckedModeBanner: false,
       home: googleMap(),
 
